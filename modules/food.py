1abd2234eb100d2331643fb25cfc89ad7f7487d1
jztech101 <jzhou2oo6@gmail.com>
2018-01-19 17:35:25 +0000

    add back lispy as owner only and rename yelp & dinner commands

--- modules/food.py
+++ modules/food.py
@@ -1,80 +1,51 @@
-#1/usr/bin/env/python
-"""
-food.py - Jenni Food Module
-by afuhrtrumpet
+#!/usr/bin/env python
+'''
+dinner.py - Dinner Module
+Copyright 2014 Sujeet Akula (sujeet@freeboson.org)
+Copyright 2013 Michael Yanovich (yanovich.net)
+Copyright 2013 Unknown
+Licensed under the Eiffel Forum License 2.
 
 More info:
+ * jenni-misc: https://github.com/freeboson/jenni-misc/
  * jenni: https://github.com/myano/jenni/
  * Phenny: http://inamidst.com/phenny/
-"""
+'''
 
-from urllib2 import HTTPError
-from urllib import quote
-from urllib import urlencode
-import random
-import requests
-import json
-import pprint
-import sys
-import urllib
-import argparse
+import re
+import web
+from modules.url import short
 
-def request(host, path, api_key, url_params=None):
-
-    url_params = url_params or {}
-    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
-    headers = {
-        'Authorization': 'Bearer %s' % api_key,
-    }
-
-    print(u'Querying {0} ...'.format(url))
-
-    response = requests.request('GET', url, headers=headers, params=url_params)
-
-    return response.json()
+re_mark = re.compile('<dt><a href="(.*?)" target="_blank">(.*?)</a></dt>')
 
 def food(jenni, input):
-    if not hasattr(jenni.config, 'yelp_apikey'):
-        return jenni.say('Please sign up for a Yelp API key to use this function.')
-    key = jenni.config.yelp_apikey
-    API_HOST = 'https://api.yelp.com'
-    SEARCH_PATH = '/v3/businesses/search'
-    BUSINESS_PATH = '/v3/businesses/'
+    '''.fd -- provide suggestions for dinner'''
+    txt = input.group(2)
+    url = 'http://www.whatthefuckshouldimakefordinner.com'
+    if txt == '-v':
+        url = 'http://whatthefuckshouldimakefordinner.com/veg.php'
+    page = web.get(url)
 
-    location = input.group(2)
-    if not location:
-        jenni.say("Please enter a location.")
-        return
-    url_params = {
-        'location': location.replace(' ', '+'),
-        'limit': 3
-    }
+    results = re_mark.findall(page)
 
-    response = request(API_HOST, SEARCH_PATH, key, url_params=url_params)
+    if results:
 
-    businesses = response.get('businesses')
+        dish = results[0][1].upper()
+        long_url = results[0][0]
 
-    if not businesses:
-        jenni.say('No businesses in ' + location + ' found.')
-        return
-    finalresponse=""
-    for x in range(3):
-        if x >= len(businesses):
-            break
-        else:
-            if not finalresponse:
-                finalresponse = businesses[x]['name']
-            else:
+        try:
+            short_url = short(long_url)[0][1]
+        except:
+            short_url = long_url
 
-                finalresponse = finalresponse + ", " + businesses[x]['name']
-    if len(businesses) <= 3:
-        jenni.say(str(len(businesses))+ " found: " + finalresponse)
+        jenni.say("WHY DON'T YOU EAT SOME FUCKING " + dish +
+                  ", HERE IS THE RECIPE: " + short_url)
+
     else:
-        jenni.say(str(len(businesses))+ " found, showing first 3: " + finalresponse)
+        jenni.say("I DON'T FUCKING KNOW, EAT PIZZA.")
 
-food.commands = ["food", "yelp"]
-food.priority = 'medium'
-food.example = '.food <location> <thing>'
+food.commands = ['food','breakfast','lunch', 'brunch','fucking_dinner', 'fuckingdinner', 'dinner', 'fd', 'wtfsimfd']
+food.priority = 'low'
 
 if __name__ == '__main__':
     print __doc__.strip()
---
+++ modules/yelp.py
@@ -0,0 +1,80 @@
+#1/usr/bin/env/python
+"""
+food.py - Jenni Food Module
+by afuhrtrumpet
+
+More info:
+ * jenni: https://github.com/myano/jenni/
+ * Phenny: http://inamidst.com/phenny/
+"""
+
+from urllib2 import HTTPError
+from urllib import quote
+from urllib import urlencode
+import random
+import requests
+import json
+import pprint
+import sys
+import urllib
+import argparse
+
+def request(host, path, api_key, url_params=None):
+
+    url_params = url_params or {}
+    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
+    headers = {
+        'Authorization': 'Bearer %s' % api_key,
+    }
+
+    print(u'Querying {0} ...'.format(url))
+
+    response = requests.request('GET', url, headers=headers, params=url_params)
+
+    return response.json()
+
+def yelp(jenni, input):
+    if not hasattr(jenni.config, 'yelp_apikey'):
+        return jenni.say('Please sign up for a Yelp API key to use this function.')
+    key = jenni.config.yelp_apikey
+    API_HOST = 'https://api.yelp.com'
+    SEARCH_PATH = '/v3/businesses/search'
+    BUSINESS_PATH = '/v3/businesses/'
+
+    location = input.group(2)
+    if not location:
+        jenni.say("Please enter a location.")
+        return
+    url_params = {
+        'location': location.replace(' ', '+'),
+        'limit': 3
+    }
+
+    response = request(API_HOST, SEARCH_PATH, key, url_params=url_params)
+
+    businesses = response.get('businesses')
+
+    if not businesses:
+        jenni.say('No businesses in ' + location + ' found.')
+        return
+    finalresponse=""
+    for x in range(3):
+        if x >= len(businesses):
+            break
+        else:
+            if not finalresponse:
+                finalresponse = businesses[x]['name']
+            else:
+
+                finalresponse = finalresponse + ", " + businesses[x]['name']
+    if len(businesses) <= 3:
+        jenni.say(str(len(businesses))+ " found: " + finalresponse)
+    else:
+        jenni.say(str(len(businesses))+ " found, showing first 3: " + finalresponse)
+
+yelp.commands = ["yelp"]
+yelp.priority = 'medium'
+yelp.example = '.food <location> <thing>'
+
+if __name__ == '__main__':
+    print __doc__.strip()
