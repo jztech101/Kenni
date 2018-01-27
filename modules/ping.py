828f672048dbae39295efb88bbdc676513f9384e
jztech101 <jzhou2oo6@gmail.com>
2018-01-27 16:36:45 +0000

    moved personal commands to it's own module

--- modules/ping.py
+++ modules/ping.py
@@ -30,26 +30,6 @@ def f_pong(jenni, input):
 f_pong.commands = ['pong']
 f_pong.priority = 'high'
 
-def potato(jenni, input):
-    jenni.write(['PRIVMSG', input.sender], '\x01ACTION is a potato')
-potato.commands = ['potato']
-potato.priority = 'high'
-
-def moo(jenni, input):
-    jenni.reply('mooooooooooo')
-moo.commands = ['moo']
-moo.priority = 'high'
-
-def cookie(jenni, input):
-    nick = input.nick
-    if input.group(2):
-        nick = input.group(2)
-    jenni.write(['PRIVMSG', input.sender], '\x01ACTION gives ' + nick + ' a cookie\x01')
-cookie.commands = ['cookie']
-cookie.priority = 'high'
 if __name__ == '__main__':
     print __doc__.strip()
-def source(jenni, input):
-    jenni.reply('https://github.com/jztech101/jenni')
-source.commands = ['source']
-source.priority = 'high'
+
