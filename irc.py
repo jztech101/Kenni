ddf9dc599492a419caf14c15bd9abf0e23cdab5c
Julian Zhou <jzhou2oo6@gmail.com>
2018-05-05 20:49:43 +0000

    Add Invite Logging (Resolves #10)

--- irc.py
+++ irc.py
@@ -319,6 +319,8 @@ class Bot(asynchat.async_chat):
                                 self.msg(self.logchan_pm, '[Notice] ' + dlist[0].replace(":","") + ': ' + ' '.join(dlist[3:]).replace(":",""), True)
                         elif dlist[1].strip() == 'PRIVMSG' and dlist[2].isalnum():
                             self.msg(self.logchan_pm, '[PM] ' + dlist[0].replace(":","") + ': ' + ' '.join(dlist[3:]).replace(":",""), True)
+                        elif dlist[1].strip() == 'INVITE':
+                            self.msg(self.logchan_pm, '[Invite] ' + dlist[0].replace(":","") + ': ' + dlist[3].replace(":",""), True)
                         else:
                             self.msg(self.logchan_pm, line, True)
             if self.logging:
