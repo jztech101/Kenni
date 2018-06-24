7b264193c6c2edafa1cf37639b7cf62138c9ad10
Julian Zhou <jzhou2oo6@gmail.com>
2018-06-24 12:15:28 -0500

    Bug Fixes

--- irc.py
+++ irc.py
@@ -313,7 +313,7 @@ class Bot(asynchat.async_chat):
                 ## send stuff in PM to logging channel
                 dlist = line.split()
                 if len(dlist) >= 3:
-                    if (not tools.isChan(dlist[2],True) or dlist[1].strip() == 'NOTICE' or dlist[1].strip() == 'PART' or dlist[1].strip() == 'KICK') and dlist[1].strip() not in IRC_CODES:
+                    if (not tools.isChan(dlist[2],True) or dlist[1].strip() == 'NOTICE'):
                         if dlist[1].strip() == 'NOTICE':
                             if tools.isChan(dlist[2],True):
                                 self.msg(self.logchan_pm, '[Notice] ' + dlist[0].replace(':','') + ': (' + dlist[2] + ') ' + ' '.join(dlist[3:]).replace(":",""), True)
@@ -323,7 +323,8 @@ class Bot(asynchat.async_chat):
                             self.msg(self.logchan_pm, '[PM] ' + dlist[0].replace(":","") + ': ' + ' '.join(dlist[3:]).replace(":",""), True)
                         elif dlist[1].strip() == 'INVITE':
                             self.msg(self.logchan_pm, '[Invite] ' + dlist[0].replace(":","") + ': ' + dlist[3].replace(":",""), True)
-                        elif dlist[1].strip() == 'PART'and dlist[0].strip().startswith(":" + self.nick):
+                    elif tools.isChan(dlist[2],True):    
+                        if dlist[1].strip() == 'PART'and dlist[0].strip().startswith(":" + self.nick):
                             if len(dlist) > 3:
                                 self.msg(self.logchan_pm, '[Part] ' + dlist[0].replace(":","") + ': (' + dlist[2] + ') '+ ' '.join(dlist[3:]).replace(":",""), True)
                             else:
@@ -333,10 +334,6 @@ class Bot(asynchat.async_chat):
                                 self.msg(self.logchan_pm, '[Kick] ' + dlist[0].replace(":","") + ': (' + dlist[2] + ') '+ ' '.join(dlist[4:]).replace(":",""), True)
                             else:
                                 self.msg(self.logchan_pm, '[Kick] ' + dlist[0].replace(":","") + ': (' + dlist[2] + ') ', True)
-            if self.logging:
-                ## if logging (to log file) is enabled
-                ## send stuff to the log file
-                log_raw(line)
 
         self.buffer = ''
 
