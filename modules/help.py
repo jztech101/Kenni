e632c10ac489ae300d5a3617c06093617a9cf59f
Julian Zhou <jzhou2oo6@gmail.com>
2018-06-26 15:00:17 +0000

    Readd help module

---
+++ modules/help.py
@@ -0,0 +1,15 @@
+def doc(kenni, input):
+    """Shows a command's documentation, and possibly an example."""
+    if input.group(2):
+        name = input.group(2)
+        if name and name in kenni.doc:
+            kenni.say(kenni.doc[name][0])
+            if kenni.doc[name][1]:
+                kenni.say('e.g. ' + kenni.doc[name][1])
+        else:
+            kenni.say('No help found')
+    else:
+        kenni.say('help: returns possibly helpful information')
+doc.commands = ['help']
+doc.example = 'help'
+doc.priority = 'low'
